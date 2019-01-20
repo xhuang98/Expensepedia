5f7055824daf90cbf7e1d3456f91f5b08a306e7f
Nasir Hemed <nasirhemed@ymail.com>
2019-01-19 22:52:03 -0500

    Add OCR stuff

---
+++ app/src/main/java/uofthacks/expensepedia/OCRRequestJSONBody.java
@@ -0,0 +1,19 @@
+package uofthacks.expensepedia;
+
+public class OCRRequestJSONBody {
+    private String url;
+
+    /**
+     * @return the url
+     */
+    public String getUrl() {
+        return url;
+    }
+
+    /**
+     * @param url the url to set
+     */
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
