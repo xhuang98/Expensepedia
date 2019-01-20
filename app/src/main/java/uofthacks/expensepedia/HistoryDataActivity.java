afa6f939650d1ffe4b3c7f1f57b9cc7773c37959
kevin <kevinyz.zhang@mail.utoronto.ca>
2019-01-20 04:41:52 -0500

    added history and may page functionality and changed android manifest

--- app/src/main/java/uofthacks/expensepedia/HistoryDataActivity.java
+++ app/src/main/java/uofthacks/expensepedia/HistoryDataActivity.java
@@ -3,6 +3,8 @@ package uofthacks.expensepedia;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ImageButton;
 
 public class HistoryDataActivity extends AppCompatActivity {
     AppController controller = AppController.getInstance();
@@ -10,6 +12,14 @@ public class HistoryDataActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.history_page);
+        ImageButton butt = findViewById(R.id.maybutt);
+
+        butt.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                startActivity(new Intent(HistoryDataActivity.this, MayActivity.class));
+            }
+        });
     }
 
 
---
+++ app/src/main/java/uofthacks/expensepedia/MayActivity.java
@@ -0,0 +1,14 @@
+package uofthacks.expensepedia;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class MayActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.may_page);
+    }
+}
